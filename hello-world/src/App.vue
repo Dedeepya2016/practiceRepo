d667bfc9a0dfa042e601d497562f8b37522692aa
dedeepya <deepudedeepya111@gmail.com>
2024-03-09 14:42:23 +0530

    new additional files

---
+++ hello-world/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div>hello world</div>
+  <h2>{{name}}</h2>
+  <div v-text="age">
+  </div>
+</div>
+</template>
+<script>
+
+export default {
+  data(){
+    return{
+      name: "Dedeepya",
+      age:"twenty five",
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  text-align: center;
+}
+</style>
